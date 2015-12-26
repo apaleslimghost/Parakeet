77237295129590af389113583663985e88e97c24
Matt Brennan <mattyb1000@gmail.com>
2015-12-26 12:40:15 +0000

    live replication

--- db/browser.js
+++ db/browser.js
@@ -2,6 +2,6 @@ var PouchDb = require('pouchdb');
 var prefs = require('../prefs');
 var db = new PouchDb('transactions')
 
-db.replicate.from(prefs.transactionsDb);
+db.replicate.from(prefs.transactionsDb, {live: true});
 
 module.exports = db;
